6e12e883965b0a63a5b4c4e079fbc8fcec68e59c
Dmytro Potapchuk <traiget@gmail.com>
2024-11-06 16:54:44 +0200

    added packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const headerStyles: React.CSSProperties = {
+    height: "64px",
+    left: "0px",
+    right: "0px",
+    position: "fixed",
+};
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <header style={headerStyles}></header>
+        </div>
+    );
 }
 
 export default App;
